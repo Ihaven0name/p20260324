e189146d3cb273e392e6675c9b8b63121fd2bfa8
Li <763345689l@gmail.com>
2026-04-20 18:33:27 +0800

    delete

---
+++ Source/p20260324/Public/Animation/AnimNotifyState/AnimNotifyState_AddStateTag.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "AnimNotifyState_AddStateTag.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class P20260324_API UAnimNotifyState_AddStateTag : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
